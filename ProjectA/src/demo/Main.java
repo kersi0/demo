27283b1fff36193d611da2344f89d36f686b506a
Kerstin Breiteneder <kerstin.breiteneder@ntswincash.com>
2012-11-21 08:19:45 +0100

    Modify text.

--- ProjectA/src/demo/Main.java
+++ ProjectA/src/demo/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on branch 'master' of https://github.com/kersi0/demo.git
 /*
  * Copyright 2012 NTS New Technology Systems GmbH. All Rights reserved.
  * NTS PROPRIETARY/CONFIDENTIAL. Use is subject to NTS License Agreement.
@@ -13,3 +14,21 @@ public class Main {
 		System.out.println("Hello World! Git is nice!");
 	}
 }
+=======
+/*
+ * Copyright 2012 NTS New Technology Systems GmbH. All Rights reserved.
+ * NTS PROPRIETARY/CONFIDENTIAL. Use is subject to NTS License Agreement.
+ * Address: Doernbacher Strasse 126, A-4073 Wilhering, Austria
+ * Homepage: www.ntswincash.com
+ */
+
+package demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello ");
+		System.out.println("World! Git is nice");
+	}
+}
+>>>>>>> ecace5f Modify text.
