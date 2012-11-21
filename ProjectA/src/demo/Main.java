eb2eb290d56b0247718071a38c2f000c9144e91f
Kerstin Breiteneder <kerstin.breiteneder@ntswincash.com>
2012-11-21 09:17:58 +0100

    Modify header.

--- ProjectA/src/demo/Main.java
+++ ProjectA/src/demo/Main.java
@@ -2,7 +2,7 @@
  * Copyright 2012 NTS New Technology Systems GmbH. All Rights reserved.
  * NTS PROPRIETARY/CONFIDENTIAL. Use is subject to NTS License Agreement.
  * Address: Doernbacher Strasse 126, A-4073 Wilhering, Austria
- * Homepage: www.ntswincrash.com
+ * Homepage: www.ntswincash.com
  */
 
 package demo;
