1d97300cdaa71025d855a4d6e3da40b5b7e1d163
Kerstin Breiteneder <kerstin.breiteneder@ntswincash.com>
2012-11-21 08:03:52 +0100

    Add ProjectA.

---
+++ ProjectA/src/demo/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2012 NTS New Technology Systems GmbH. All Rights reserved.
+ * NTS PROPRIETARY/CONFIDENTIAL. Use is subject to NTS License Agreement.
+ * Address: Doernbacher Strasse 126, A-4073 Wilhering, Austria
+ * Homepage: www.ntswincash.com
+ */
+
+package demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
