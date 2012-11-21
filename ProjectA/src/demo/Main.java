529a644b8486e2b0d1c1310f15daec38ecee35d6
Kerstin Breiteneder <kerstin.breiteneder@ntswincash.com>
2012-11-21 08:45:34 +0100

    Add Hello SacoTeam to main.

--- ProjectA/src/demo/Main.java
+++ ProjectA/src/demo/Main.java
@@ -12,5 +12,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Kerstin!");
 		System.out.println("World! Git is nice");
+		System.out.println("Hello SaCo team.");
 	}
 }
