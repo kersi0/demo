7bb4cff75294ef280b115d2f9a5b9a1a03c5e950
Christoph Wimberger <christoph.wimberger@ntswincash.com>
2012-11-21 09:06:49 +0100

    Change things.

--- ProjectA/src/demo/Main.java
+++ ProjectA/src/demo/Main.java
@@ -12,6 +12,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Kerstin!");
 		System.out.println("World! Git is nice");
-		System.out.println("Hello SaCo team.");
+		System.out.println("Hello SaCo team. Things happen.");
 	}
 }
