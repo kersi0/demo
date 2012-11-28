abf30139aed8356ba59b04d83d5e8ec2af3e56c0
Christoph Wimberger <christoph.wimberger@ntswincash.com>
2012-11-28 10:45:56 +0100

    fasdfasd

--- ProjectA/src/demo/Main.java
+++ ProjectA/src/demo/Main.java
@@ -15,6 +15,6 @@ public class Main {
 		System.out.println("Hello SaCo team and christoph.");
 		System.out.println("Things happen.");
 		System.out.println("Test.sdfsjdkfsjkldjsklsd");
-		System.exit(2);
+		System.exit(436);
 	}
 }
